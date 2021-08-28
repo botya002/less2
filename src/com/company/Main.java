1d7efd903044a4af64e7fd1afe062cd6dc88eba1
botya02 <botagejm@gmail.com>
2021-08-28 12:38:31 +0600

    Добавлено исправление

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         int age = generateRandomAge();//random number
-        temp(21, 34);
-        temp(age,45 );
+        temperature(21, 34);
+        temperature(age,45 );
     }
 //home work 1
-    private static int temp (int age, int temp) {
-        if (age > 20 && age < 45 && temp > 30) {
+
+    public static String temperature(int age, int temperature) {
+        String result = "";
+        if (age > 20 && age < 45 && temperature > 30) {
             System.out.println("Можно идти гулять");
-        } else if (age < 20 && temp <= 28) {
+        } else if (age < 20 && temperature <= 28) {
             System.out.println("Можно идти гулять 2");
-        } else if (age > 45 && temp < 25) {
+        } else if (age > 45 && temperature < 25) {
             System.out.println("Можно идти гулять 3");
         } else {
             System.out.println("Оставайтесь дома");
         }
-        return temp;
+        return result;
     }
+
 
 //home work 2
     private static int generateRandomAge() {
